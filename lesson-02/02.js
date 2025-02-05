f17a3c0ad9e26c716f6411fb55df68ab51bd85eb
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-05 11:49:25 +0300

    Variant1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,9 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1; // your code
+let dice2; // your code
+let isWinningDouble; // your code
 
 /*
 console.log('Первый бросок: ' + dice1)
@@ -20,3 +20,15 @@ if (isWinningDouble) {
   console.log('Не выигрышный дубль.')
 }
 */
+
+let dice1 = Math.floor(Math.random() * 6) + 1; // Первый бросок
+let dice2 = Math.floor(Math.random() * 6) + 1; // Второй бросок
+let isWinningDouble = dice1 === dice2 && dice1 > 3; // Проверка на выигрышный дубль
+
+console.log("Первый бросок: " + dice1);
+console.log("Второй бросок: " + dice2);
+if (isWinningDouble) {
+  console.log("Выигрышный дубль!");
+} else {
+  console.log("Не выигрышный дубль.");
+}
