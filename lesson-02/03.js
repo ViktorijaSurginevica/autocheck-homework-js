a9578cfabed8a26deee3aaa3c2d91910698c18be
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-05 12:13:53 +0300

    Variant1

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,22 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = 70; // тестовое значение, можно изменять
+let grade;
+
+if ((grade = 0 && grade < 50)) {
+  console.log("F");
+}
+if (grade > 49 && grade < 70) {
+  console.log("D");
+}
+if (grade > 69 && grade < 80) {
+  console.log("C");
+}
+if (grade > 79 && grade < 90) {
+  console.log("C");
+} else {
+  console.log("A");
+}
+
 // your code
