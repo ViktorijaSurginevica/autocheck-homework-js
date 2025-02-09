34b98bc62b5775dc3a8499195d52f921510f270d
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-09 13:29:51 +0300

    lesson2-6

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,12 +2,5 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(num) {  
-    return num % 2 === 0; // Если остаток от деления на 2 равен 0, возвращаем true  
+    return num % 2 === 0;  
 }  
-
-// Примеры использования  
-console.log(isEven(4)); // true  
-console.log(isEven(5)); // false  
-console.log(isEven(0)); // true  
-console.log(isEven(-2)); // true  
-console.log(isEven(-3)); // false
