672964eb14b1651c87c4b5ebcf126097ba639f5b
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-09 14:33:07 +0300

    rm default from 01.js

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,6 +1,6 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage(num1, num2) {  
-    // Вычисляем среднее арифметическое  
-    return (num1 + num2) / 2;  
-}  result = prompt(title, [default]);
+function calculateAverage(num1, num2) {
+  // Вычисляем среднее арифметическое
+  return (num1 + num2) / 2;
+}
