e51656f9ab7950dab7033aac9ab1bbdf5825de5c
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-09 15:49:22 +0300

    lasr version

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,16 +1,11 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(num1, num2, num3) {
-  // Сравниваем num1 с num2 и num3, чтобы узнать, является ли он наибольшим.
   if (num1 >= num2 && num1 >= num3) {
     return num1;
-  }
-  // Сравниваем num2 с num1 и num3, чтобы узнать, является ли он наибольшим.
-  else if (num2 >= num1 && num2 >= num3) {
+  } else if (num2 >= num1 && num2 >= num3) {
     return num2;
-  }
-  // Если ни num1, ни num2 не являются наибольшими, то num3 должен быть наибольшим.
-  else {
+  } else {
     return num3;
   }
 }
