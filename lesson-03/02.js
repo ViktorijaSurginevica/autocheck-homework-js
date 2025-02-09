588a8a58b93e326a3e4afa52c9286f402be3979d
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-09 13:25:38 +0300

    new-try

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,6 +1,13 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven(number) {  
-    return (number / 2) === Math.floor(number / 2);  
-}
+function isEven(num) {  
+    return num % 2 === 0; // Если остаток от деления на 2 равен 0, возвращаем true  
+}  
+
+// Примеры использования  
+console.log(isEven(4)); // true  
+console.log(isEven(5)); // false  
+console.log(isEven(0)); // true  
+console.log(isEven(-2)); // true  
+console.log(isEven(-3)); // false
