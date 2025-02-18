6b92e48cf899904301d5bb73b4ae07fa03b36144
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-18 22:48:43 +0300

    no comments -lesson1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -16,8 +16,8 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 function includesElement(array, element) {  
     for (let i = 0; i < array.length; i++) {  
         if (array[i] === element) {  
-            return true; // элемент найден  
+            return true; 
         }  
     }  
-    return false; // элемент отсутствует  
+    return false; 
 }  
