7a8f0bf2ecbfe2f10c55a1add523feccd63b7e4e
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-02 13:09:29 +0300

    game object

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -16,13 +16,11 @@ const game = {
     lumber: 100,
   },
   addResource(resource, amount) {
-    if (this.resources.hasOwnProperty(resource)) {  
-      // Если ресурс существует, добавляем к нему количество  
-      this.resources[resource] += amount;  
-      console.log(`${amount} ${resource} added. New amount: ${this.resources[resource]}`);  
-    } else {  
-      // Если ресурс не существует, выводим сообщение об ошибке  
-      console.log("Invalid resource");  
-    }  
+   if(game.resources.hasOwnProperte(resource))
+    console.log(game.resources[resource] = game.resources[resource] + amount);
+    
+    }  else {
+      console.log("Invalid resource");
+      
+    }
   }  
-}
