eec14378251e7b705381caed0e19a6481e3a3962
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-16 10:28:03 +0300

    For checking

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -13,7 +13,36 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg",
+];
+
+// Получаем ссылки на элементы кнопок и изображение
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const imageElement = document.getElementById("web-tech-image");
+
+// Инициализируем индекс текущего изображения
+let currentIndex = 0;
+
+// Функция для обновления отображаемого изображения
+function updateImage() {
+  imageElement.src = WEB_TECH_IMAGES[currentIndex];
+}
+
+// Обработчик события для кнопки "prev"
+prevButton.addEventListener("click", () => {
+  currentIndex =
+    (currentIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
+  updateImage();
+});
+
+// Обработчик события для кнопки "next"
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % WEB_TECH_IMAGES.length;
+  updateImage();
+});
+
+// Инициализация изображения при загрузке
+updateImage();
