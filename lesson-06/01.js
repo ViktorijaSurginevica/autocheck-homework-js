9ff55fe69e3fd373d8f0a2e4ec9044607d503f09
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-16 10:46:09 +0300

    variant4

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -11,30 +11,33 @@
 
 🧙‍♂️ Совет: обратите внимание на управление индексом текущего изображения — это ключ к успешному переключению изображений.
 */
-const WEB_TECH_IMAGES = [  
-  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg",  
-  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg",  
-  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg",  
-];  
 
-let currentIndex = 0;  
-const imageElement = document.getElementById('web-tech-image');  
+document.addEventListener('DOMContentLoaded', () => {  
+  const WEB_TECH_IMAGES = [  
+    "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg",  
+    "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg",  
+    "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg",  
+  ];  
 
-function updateImage() {  
-  imageElement.src = WEB_TECH_IMAGES[currentIndex];  
-}  
+  let currentIndex = 0;  
+  const imageElement = document.getElementById('web-tech-image');  
 
-function handlePrevClick() {  
-  currentIndex = (currentIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;  
+  function updateImage() {  
+    imageElement.src = WEB_TECH_IMAGES[currentIndex];  
+  }  
+
+  function handlePrevClick() {  
+    currentIndex = (currentIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;  
+    updateImage();  
+  }  
+
+  function handleNextClick() {  
+    currentIndex = (currentIndex + 1) % WEB_TECH_IMAGES.length;  
+    updateImage();  
+  }  
+
+  document.getElementById('prev').addEventListener('click', handlePrevClick);  
+  document.getElementById('next').addEventListener('click', handleNextClick);  
+
   updateImage();  
-}  
-
-function handleNextClick() {  
-  currentIndex = (currentIndex + 1) % WEB_TECH_IMAGES.length;  
-  updateImage();  
-}  
-
-document.getElementById('prev').addEventListener('click', handlePrevClick);  
-document.getElementById('next').addEventListener('click', handleNextClick);  
-
-updateImage();  
+});  
