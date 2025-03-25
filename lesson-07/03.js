6266f33ea1cbc4592965b50e3b9ed7042dfd2dd1
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-25 20:53:40 +0300

    lesson 3- 2

--- lesson-07/03.js
+++ lesson-07/03.js
@@ -10,5 +10,7 @@
 */
 
 function truncate(str, maxLength) {  
-  return str.length > maxLength ? str.slice(0, maxLength - 3) + "..." : str;  
+  return str.length > maxLength  
+      ? `${str.slice(0, maxLength - 3)}...`  
+      : str;  
 }  
