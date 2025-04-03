09e9fa82bc786b5ac25f37a25beb3c8e9c3a2268
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-04-03 15:21:55 +0300

    variant 1

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,13 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i)) {
+      // Если callback возвращает true
+      result.push(array[i]); // Добавляем элемент в новый массив
+    }
+  }
+  return result;
+};
